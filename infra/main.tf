21cfd63260a1d9b7b987ab193e0f8eb59a9936cb
azrubael <i0638464000@gmail.com>
2024-10-14 17:39:51 +0300

    Added -- 'infra/main.tf'.

---
+++ infra/main.tf
@@ -0,0 +1,32 @@
+terraform {
+    requied_roviders {
+        google = {
+            source = "hashicorp/google"
+            version = "4.27.0"
+        }
+    }
+}
+
+provider "google" {
+    # Configuration options
+    project = "example"
+    region = "us-central1"
+}
+
+resource "google_compute_instance" "terraform" {
+    name = "terraform"
+    machine_type = "n1-standard-2"
+    zone = "us-central1-c"
+
+    boot_disk {
+        initialize_params {
+            image = "debian-cloud/debian-11"
+        }
+    }
+
+    network_interface {
+        network = "default"
+        access_config {
+        }
+    }
+}

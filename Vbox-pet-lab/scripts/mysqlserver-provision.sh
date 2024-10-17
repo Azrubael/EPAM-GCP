89f1c2f4cfb5de9159797ab1b8cd02bfeb9e2167
azrubael <i0638464000@gmail.com>
2024-10-17 19:14:25 +0300

    Updated after the first unsuccessful MySQL start.

--- Vbox-pet-lab/scripts/mysqlserver-provision.sh
+++ Vbox-pet-lab/scripts/mysqlserver-provision.sh
@@ -1,25 +1,45 @@
 #!/bin/bash
 
+set -e  # Stop the script if an error happened
+
 sudo apt-get update
+
+echo
+echo "### [1] -- Installind dependencies"
 sudo apt-get install -y wget lsb-release gnupg
+
+echo
+echo "### [2] -- Downloading MySQL APT config..."
 wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb
+
+echo
+echo "### [3] -- Installing MySQL APT config..."
 sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb
 sudo apt-get update
+
+echo
+echo "### [4] -- Installing MySQL Server..."
 # sudo apt-get install -y mysql-server
 sudo apt-get install -y mysql-server=8.0.39-0ubuntu0.20.04.1
 
-### Check the installed MySQL
+echo
+echo
+echo "### [5] -- Checking the MySQL version"
 mysql --version
 
+echo
+echo "### [6] -- Starting MySQL as a service..."
 sudo systemctl start mysql
 sudo systemctl enable mysql
 
+
+echo
+echo "### [7] -- Run SQL commands to create a user and a database"
 MYSQL_ROOT_PASSWORD="newpassword"
 MYSQL_USER="petclinic"
 MYSQL_PASSWORD="petclinic"
 MYSQL_DATABASE="information_schema"
 
-### Run SQL commands to create a user and a database
 sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';

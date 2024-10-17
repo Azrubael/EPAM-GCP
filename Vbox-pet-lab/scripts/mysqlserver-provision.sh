7229074863ae07fa9c52c8b2800fec000aa91d95
azrubael <i0638464000@gmail.com>
2024-10-17 23:14:38 +0300

    Updated -- 'Vbox-pet-lab/scripts/*'. With *.env provision.

--- Vbox-pet-lab/scripts/mysqlserver-provision.sh
+++ Vbox-pet-lab/scripts/mysqlserver-provision.sh
@@ -5,40 +5,35 @@ set -e  # Stop the script if an error happened
 sudo apt-get update
 
 echo
-echo "### [1] -- Installind dependencies"
+echo "### [1] -- Installing MySQL 8.0 dependencies"
 sudo apt-get install -y wget lsb-release gnupg
 
 echo
-echo "### [2] -- Downloading MySQL APT config..."
-wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb
+echo "### [2] -- Installing MySQL Server..."
+sudo apt-get install -y mysql-server
 
 echo
-echo "### [3] -- Installing MySQL APT config..."
-sudo dpkg -i mysql-apt-config_0.8.22-1_all.deb
-sudo apt-get update
-
-echo
-echo "### [4] -- Installing MySQL Server..."
-# sudo apt-get install -y mysql-server
-sudo apt-get install -y mysql-server=8.0.39-0ubuntu0.20.04.1
-
-echo
-echo
-echo "### [5] -- Checking the MySQL version"
+echo "### [3] -- Checking the MySQL version..."
 mysql --version
 
 echo
-echo "### [6] -- Starting MySQL as a service..."
+echo "### [4] -- Loading the environment..."
+source /home/vagrant/mysqlserver.env
+
+
+echo
+echo "### [5] -- Starting MySQL as a service..."
 sudo systemctl start mysql
 sudo systemctl enable mysql
 
 
 echo
-echo "### [7] -- Run SQL commands to create a user and a database"
-MYSQL_ROOT_PASSWORD="newpassword"
-MYSQL_USER="petclinic"
-MYSQL_PASSWORD="petclinic"
-MYSQL_DATABASE="information_schema"
+echo "### [6] -- Run SQL commands to create a user and a database"
+# MYSQL_ROOT_PASSWORD=""
+# MYSQL_ALLOW_EMPTY_PASSWORD=true
+# MYSQL_USER="petclinic"
+# MYSQL_PASSWORD="petclinic"
+# MYSQL_DATABASE="information_schema"
 
 sudo mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
@@ -48,17 +43,6 @@ FLUSH PRIVILEGES;
 EOF
 
 
-: << '#AlternativeSettngUpMySQL'
-### Команда 'mysql_secure_installation' предназначена для настройки безопасности MySQL, включая установку пароля для пользователя root, удаление анонимных пользователей и другие параметры безопасности. В данном случае устанавливается пароль для пользователя root как PetClinic, вместо уже установленной MYSQL_ROOT_PASSWORD.
-
-sudo mysql_secure_installation <<EOF
-
-y
-PetClinic
-PetClinic
-y
-y
-y
-y
-EOF
-#AlternativeSettngUpMySQL
+echo
+echo "### [7] -- MySQL service status"
+sudo systemctl status mysql
--- Vbox-pet-lab/scripts/petclinic-provision.sh
+++ Vbox-pet-lab/scripts/petclinic-provision.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+set -e  # Stop the script if an error happened
+
 sudo apt-get update
 sudo apt-get install -y openjdk-17-jre
 mkdir -p /app
 mv /home/vagrant/spring-petclinic.jar /app/spring-petclinic.jar
 mv /home/vagrant/__cacert_entrypoint.sh /app/__cacert_entrypoint.sh
 mv /home/vagrant/start_app.sh /app/start_app.sh
+mv /home/vagrant/petclinic.env /app/petclinic.env
 sudo mv /home/vagrant/petclinic.service /etc/systemd/system/petclinic.service
 
+source /app/petclinic.env
 
 sudo systemctl daemon-reload
 sudo systemctl enable petclinic.service

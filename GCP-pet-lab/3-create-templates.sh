d8b5a34e5be42318b2873c8017c84ed850e9c38f
azrubael <i0638464000@gmail.com>
2024-10-19 16:09:09 +0300

    Added and tested in GCP -- 'GCP-pet-lab/3-create-templates.sh'.

--- GCP-pet-lab/3-create-templates.sh
+++ GCP-pet-lab/3-create-templates.sh
@@ -3,13 +3,13 @@
 source ./.env/env_local
 MY_BUCKET="${GCP_PROJECT_ID}-bucket"
 MY_VPC="petclinic-vpc"
-MY_FILES=(  ".env/env_local"
-            ".env/petclinic.env"
-            ".env/mysqlserver.env"
+MY_FILES=(  "app/spring-petclinic.jar"
             "app/__cacert_entrypoint.sh"
             "app/start_app.sh"
             "app/petclinic.service"
-            "app/spring-petclinic.jar" )
+            ".env/env_local"
+            ".env/petclinic.env"
+            ".env/mysqlserver.env" )
 
 PC_SUBNET="pc-subnet"
 PC_FIREWALL="petclinic-firewall"
@@ -51,19 +51,34 @@ gcloud compute firewall-rules create allow-petclinic-from-internet \
     --project="$GCP_PROJECT_ID" \
     --direction=INGRESS \
     --action=ALLOW \
-    --rules=tcp:80,tcp:22 \
+    --network="$MY_VPC" \
+    --rules=tcp:8080 \
+    --priority=1000 \
     --source-ranges=0.0.0.0/0 \
     --target-tags="$PC_FIREWALL" \
-    --description="Allow HTTP and HTTPS traffic to petclinic"
+    --description="Allow HTTP traffic to petclinic"
 
 gcloud compute firewall-rules create allow-mysql-from-petclinic \
     --project="$GCP_PROJECT_ID" \
     --direction=INGRESS \
     --action=ALLOW \
-    --rules=tcp:3306,tcp:22 \
+    --network="$MY_VPC" \
+    --rules=tcp:3306 \
+    --priority=1000 \
     --source-ranges=10.0.1.0/24 \
     --target-tags="$SQL_FIREWALL" \
     --description="Allow MySQL traffic from petclinic to mysqlserver"
+
+gcloud compute firewall-rules create allow-ssh-from-internet \
+    --project="$GCP_PROJECT_ID" \
+    --direction=INGRESS \
+    --action=ALLOW \
+    --network="$MY_VPC" \
+    --rules=tcp:22,icmp \
+    --priority=999 \
+    --source-ranges=0.0.0.0/0 \
+    --target-tags="$PC_FIREWALL","$SQL_FIREWALL" \
+    --description="Allow SSH traffic from internet."
 
 
 echo
@@ -77,13 +92,12 @@ gcloud compute instances create $PC_SRV \
     --provisioning-model=STANDARD \
     --service-account="$GCP_SERVICE_ACCOUNT" \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
-    --tags=http-server,"$PC_SRV",lb-health-check \
-    --create-disk=boot=yes,device-name="$PC_SRV",image=projects/debian-cloud/global/images/debian-11-bullseye-v20241009,mode=rw,size=10,type=pd-standard \
-    --boot-disk-device-name=petclinic-disk \
+    --tags=http-server,"$PC_FIREWALL","$PC_SRV",lb-health-check \
+    --create-disk=boot=yes,device-name="${PC_SRV}-disk",image=projects/debian-cloud/global/images/debian-11-bullseye-v20241009,mode=rw,size=10,type=pd-standard \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
-    --labels=instance="$PC_SRV-instance" \
+    --labels=app=petclinic \
     --reservation-affinity=any \
     --metadata gs_bucket="$MY_BUCKET" \
     --metadata-from-file=startup-script=scripts/petclinic-provision.sh
@@ -100,13 +114,12 @@ gcloud compute instances create $SQL_SRV \
     --provisioning-model=STANDARD \
     --service-account="$GCP_SERVICE_ACCOUNT" \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
-    --tags=db-server,"$SQL_SRV",lb-health-check \
-    --create-disk=boot=yes,device-name="$SQL_SRV",image=projects/debian-cloud/global/images/debian-11-bullseye-v20241009,mode=rw,size=10,type=pd-standard \
-    --boot-disk-device-name=mysqlserver-disk \
+    --tags=db-server,"$SQL_FIREWALL","$SQL_SRV",lb-health-check \
+    --create-disk=boot=yes,device-name="${SQL_SRV}-disk",image=projects/debian-cloud/global/images/debian-11-bullseye-v20241009,mode=rw,size=10,type=pd-standard \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
-    --labels=instance="$SQL_SRV-instance" \
+    --labels=app=petclinic \
     --reservation-affinity=any \
     --metadata gs_bucket="$MY_BUCKET" \
     --metadata-from-file=startup-script=scripts/mysqlserver-provision.sh
@@ -122,12 +135,23 @@ waiting $total_duration $interval $elapsed
 
 
 echo
-echo """### Step 8 -- Cleanup. Remove the created
-$MY_BUCKET, $MY_VPCL, and subnets $PC_SUBNET and $SQL_SUBNET"""
+echo """### Step 8 -- Removing the servers $PC_SRV, $SQL_SRV,
+the bucket $MY_BUCKET, the VPC $MY_VPC with subnets $PC_SUBNET and $SQL_SUBNET"""
 if {
+    gcloud compute instances delete $PC_SRV \
+        --project="$GCP_PROJECT_ID" \
+        --zone="$GCP_ZONE" \
+        --quiet \
+        --keep-disks=boot
+    gcloud compute instances delete $SQL_SRV \
+        --project="$GCP_PROJECT_ID" \
+        --zone="$GCP_ZONE" \
+        --quiet \
+        --keep-disks=boot
     gsutil -m rm -r gs://$MY_BUCKET/
     gcloud compute firewall-rules delete allow-petclinic-from-internet --quiet
     gcloud compute firewall-rules delete allow-mysql-from-petclinic --quiet
+    gcloud compute firewall-rules delete allow-ssh-from-internet --quiet
     gcloud compute networks subnets delete "$PC_SUBNET" \
         --region $GCP_REGION --quiet
     gcloud compute networks subnets delete "$SQL_SUBNET" \
@@ -138,4 +162,9 @@ if {
 else
     echo "Something didn't removed."
 fi
+
+
+echo
+echo "### Step 9 -- Listing available disks:"
+gcloud compute disks list --project="$GCP_PROJECT_ID"
 
